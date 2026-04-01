0242e519273640880f55f7fa1165f12530a19eb7
Tsipar Bohdana <tsiparb@gmail.com>
2026-04-01 14:08:09 +0300

    Fix test for Step 6

--- app.test.js
+++ app.test.js
@@ -18,7 +18,7 @@ describe("Library API Tests", () => {
 
   test("GET /fines/:userId should return user fines", async () => {
     const res = await request(app).get("/fines/1");
-    expect(res.statusCode).toBe(600);
+    expect(res.statusCode).toBe(200);
     expect(Array.isArray(res.body)).toBe(true);
   });
 });
