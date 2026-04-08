92290d5661dbe3ace84f84a084818c8caebabc5a
Tsipar Bohdana <tsiparb@gmail.com>
2026-04-08 12:38:19 +0300

    Introduce error

--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
 const sum = require("./index");
-test("adds 1 + 2 to equal 3", () => {
-  expect(sum(1, 2)).toBe(3);
+test("intentional fail", () => {
+  expect(2 + 2).toBe(10); // Це помилка, бо 2+2=4
 });
